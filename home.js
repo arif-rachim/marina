96060e2b811e055885c734464af3b0adc6b12c05
yallajs <a.arif.r@gmail.com>
2018-05-16 05:53:17 +0000

    update changes cetc

--- home.js
+++ home.js
@@ -16,6 +16,15 @@ const getLatestArticles = () => {
     });
 }
 
+const getArticleTags = () => {
+    return new Promise(resolve => {
+        database.articles.find({}).projection({ tags: 1}).exec((err,tags) => {
+            tags = tags.map(tag => tag.tags).filter((value,index,self) => self.indexOf(value) == index);
+            resolve(tags);
+        });
+    });
+}
+
 const getHightlightArticles  = () => {
     return new Promise(resolve => {
         database.articles.find({}).limit(20).exec((err,articles) => {
@@ -31,12 +40,12 @@ const renderArticles = (articles) => {
             content = `${content.substring(0,CONTENT_MAX_CHARACTERS) }...`
         }
         return `
-    <article style="border-bottom: 1px solid #DDD; margin-bottom: 1.5em; padding-bottom: 0.5em;">
-        <a href="${article.external_url}" target="_blank">
-            <img src="${article.image_url}" style="width: 100%;border-radius: 20px;background-color: #ddd">
+    <article style="border-bottom: 1px solid #F0F0F0; margin-bottom: 1.5em; padding-bottom: 0.5em;">
+        <a href="${article.source}" target="_blank">
+            <img src="${article.featureImage}" style="width: 100%;border-radius: 20px;background-color: #ddd">
         </a>
-        <h1 style="font-weight: 400; font-size: 1.8em; line-height: 1.4;"><a href="${article.external_url}" target="_blank" style="text-decoration: none; color: black;">${article.title}</a></h1>
-        <h3 style="font-size: 0.8em; color: #888; padding-top: 0em; padding-bottom: 0.8em;">By ${article.autor} on ${article.published_date}</h3>
+        <h1 style="font-weight: 400; font-size: 1.8em; line-height: 1.4;"><a href="${article.source}" target="_blank" style="text-decoration: none; color: black;">${article.title}</a></h1>
+        <h3 style="font-size: 0.8em; color: #888; padding-top: 0em; padding-bottom: 0.8em;">By ${article.tags} on ${article.date}</h3>
         <div style="width: 100%">
             ${content}
         </div>
@@ -55,8 +64,8 @@ const renderLatestPost = (articles) => {
         }
         return `
         <article style="padding-top: 0.5em; padding-bottom: 0.5em;">
-            <h1 style="font-weight: 700; font-style: italic; margin: 6px 0; font-size: 1.3125em; line-height: 1.143;"><a href="${article.external_url}" target="_blank" style="text-decoration: none; color: black;">${article.title}</a></h1>
-            <h3 style="font-size: 0.8em; color: #888; padding-top: 0.1em; padding-bottom: 0.1em;">BY ${article.autor} on ${article.published_date}</h3>
+            <h1 style="font-weight: 700; font-style: italic; margin: 6px 0; font-size: 1.3125em; line-height: 1.143;"><a href="${article.source}" target="_blank" style="text-decoration: none; color: black;">${article.title}</a></h1>
+            <h3 style="font-size: 0.8em; color: #888; padding-top: 0.1em; padding-bottom: 0.1em;">BY ${article.tags} on ${article.date}</h3>
             <p>
                 ${content}
             </p>
@@ -73,18 +82,18 @@ const renderHighlightStories = (articles) => {
             content = `${content.substring(0,maxChars) }...`
         }
         return `
-        <article style="margin-bottom: 1em;">
-            <h1 style="font-weight: 700; font-size: 0.9em; line-height: 1.8em; text-transform: uppercase;"><a href="${article.external_url}" target="_blank" style="text-decoration: none; color: black;">${article.title}</a></h1>
-            <h3 style="font-size: 0.7em; text-transform: uppercase;">BY ${article.autor} on ${article.published_date}</h3>
+        <article style="border-bottom: 1px solid #F0F0F0; margin-bottom: 1.5em; padding-bottom: 0.5em;">
+            <h1 style="font-weight: 700; font-size: 0.9em; line-height: 1.8em; text-transform: uppercase;"><a href="${article.source}" target="_blank" style="text-decoration: none; color: black;">${article.title}</a></h1>
+            <h3 style="font-size: 0.7em; text-transform: uppercase;">BY ${article.tags} on ${article.date}</h3>
             <p style="font-size: 0.9em;">${content}</p>
         </article>
     `};
     return articles.map(renderArticle).join('');
 }
 
+const renderTag = (tag) => `<li style="padding: 0.8em;">${tag}</li>`
 
-
-const render = ({articles}) => `
+const render = ({articles,latestArticles,highlightArticles,tags}) => `
 <!DOCTYPE html>
 <html>
 <head>
@@ -103,29 +112,28 @@ const render = ({articles}) => `
 </section>
 
 <menu style="border-top: 2px solid #333; border-bottom: 1px solid #CCC;">
-    <ul style=" display: flex; justify-content: center;">
-        <li style="padding: 0.8em;">Events</li>
-        <li style="padding: 0.8em;">Contacts</li>
+    <ul style=" display: flex; justify-content: center;flex-wrap: wrap;">
+        ${tags.map(renderTag).join('')}
         <li style="padding: 0.8em;">Home</li>
     </ul>
 </menu>
 
 <section style="padding-top: 1em; display: flex;">
     <!-- This is the left side content for displaying latest post -->
-    <aside style="width: 20%">
+    <aside style="width: 20%;box-sizing: border-box;">
         <section style="font-weight: 700;color: #666666;line-height: 1.3;font-size: 1em;">Latest Posts</section>
         <section>
             ${renderLatestPost(articles)}
         </section>
     </aside>
     <!-- This is main content to display main stories -->
-    <main style="width: 60%; padding-right: 1em; padding-left: 1em">
+    <main style="width: 60%;box-sizing: border-box; padding-right: 1em; padding-left: 1em; border-right:1px solid #F0F0F0; border-left:1px solid #F0F0F0; margin-right : 1em;margin-left: 1em">
         <section>
             ${renderArticles(articles)}
         </section>
     </main>
     <!-- This is right side content for displaying highlight stories -->
-    <aside style="width:20%">
+    <aside style="width:20%;box-sizing: border-box;">
         <section>
             ${renderHighlightStories(articles)}
         </section>
@@ -145,5 +153,6 @@ module.exports = async function(req,res) {
     let articles = await getArticles();
     let latestArticles = await getLatestArticles();
     let highlightArticles = await getHightlightArticles();
-    res.end(render({articles}))
+    let tags = await getArticleTags();
+    res.end(render({articles,latestArticles,highlightArticles,tags}))
 }
