52a00f3dfe896c0b559c1209a7803d6bb56321ab
Arif Rachim <arif@Arifs-MacBook-Pro.local>
2018-05-16 17:25:11 +0400

    Refactor document

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const api = require("./lib/api.js");
 const app = express();
 const home = require("./home.js");
 const PORT = 8000;
@@ -11,37 +10,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/v1/:resource', (req,res) => {
-    if('_mode' in req.query){
-        let mode = req.query._mode;
-        switch (mode){
-            case 'design':
-                api.postDesign(req,res);
-                break;
-            default :
-                console.error(`Warning no implementation for POST method with mode ${mode}`);
-                break;
-        }
-    } else{
-        api.post(req,res);
+    let mode = req.query._mode || 'json';
+    try{
+        require(`./lib/${mode}/post.js`).call(null,req,res);
+    }catch(err){
+        res.end(JSON.stringify(err));
     }
 });
 
 app.get('/v1/:resource', (req,res) => {
-    if('_mode' in req.query){
-        let mode = req.query._mode;
-        switch (mode){
-            case 'design':
-                api.getDesign(req,res);
-                break;
-            case 'form':
-                api.getForm(req,res);
-                break;
-            default :
-                console.error(`Warning no implementation for GET method with mode ${mode}`);
-                break;
-        }
-    } else{
-        api.get(req,res);
+    let mode = req.query._mode || 'json';
+    try{
+        require(`./lib/${mode}/get.js`).call(null,req,res);
+    }catch(err){
+        res.end(JSON.stringify(err));
     }
 });
 
