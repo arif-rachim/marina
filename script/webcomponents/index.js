6f64c06b6f7a8d9d6995a79c63a219d11c4cc696
yallajs <a.arif.r@gmail.com>
2018-05-24 04:28:58 +0000

    update webcomponent es5

--- script/webcomponents/index.js
+++ script/webcomponents/index.js
@@ -15,6 +15,7 @@ module.exports = (req,res) => {
             <!-- Meta Description -->
             <meta name="description" content="Simple page to test the webcomponent">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/1.2.0/custom-elements-es5-adapter.js"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/1.2.0/webcomponents-sd-ce.js"></script>
         </head>
         <body style="padding:1em">
