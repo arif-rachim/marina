3907b21ce1322665be16c5e67e584ac7210bb9a6
arif <a.arif.r@gmail.com>
2018-07-29 18:49:00 +0400

    update changes

--- script/page/panels/card.js
+++ script/page/panels/card.js
@@ -1,9 +1,11 @@
 module.exports = async (req,{title,content,headerBorder=false}) => {
     return `
 <div class="card">
+    <!--
     <div class="card-header">
         <h4 class="card-title">${title}</h4>
         <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+        
         <div class="heading-elements">
             <ul class="list-inline mb-0">
                 <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
@@ -11,7 +13,9 @@ module.exports = async (req,{title,content,headerBorder=false}) => {
                 <li><a data-action="close"><i class="ft-x"></i></a></li>
             </ul>
         </div>
+        
     </div>
+    -->
     <div class="card-content collapse show">
         <div class="card-body ${headerBorder ? 'border-top-blue-grey border-top-lighten-5' : ''}">
             <div class="card-text">
